be93df97c3b60c7135516fb406f0b6322f30f50c
ShayaanJ <jamilshayaan@gmail.com>
2022-07-25 00:53:49 +0500

    first

--- mask_to_bbox.py
+++ mask_to_bbox.py
@@ -1,3 +1,4 @@
+from nis import cat
 import os
 import shutil
 from cv2 import IMREAD_COLOR
@@ -8,7 +9,7 @@ from tqdm import tqdm
 from skimage.measure import label, regionprops, find_contours
 import rasterio
 
-image_size = (720, 720)
+image_size = (1920//2, 1080//2)
 
 def show_img(img, title="Img"):
     img = cv2.resize(img, image_size)
@@ -89,6 +90,7 @@ def combining_two_masks_or(mask1, mask2):
 
 def looping_over_bb_boxes(bboxes, x, threshold, color_bbox=(255,0, 0)):
     string = ""
+    invalid_bboxes = 0
     for bbox in bboxes:
         class_id = 0
         top_lft_x, top_lft_y, bot_rgt_x, bot_rgt_y = bbox
@@ -101,6 +103,11 @@ def looping_over_bb_boxes(bboxes, x, threshold, color_bbox=(255,0, 0)):
             ctr_x = (top_lft_x + bot_rgt_x) / 2
             ctr_y = (top_lft_y + bot_rgt_y) / 2
             string += "{} {} {} {} {} \n".format(class_id, ctr_x, ctr_y, width, height)
+        else:
+            invalid_bboxes += 1
+
+    print("Invalid bboxes: {}".format(invalid_bboxes))
+    print("Valid bboxes: {}".format(len(bboxes) - invalid_bboxes))
     return (string, x)
 
 def preparing_data(path):
@@ -125,6 +132,7 @@ def preparing_data(path):
 def preparing_results_dir(path):
     create_dir(path+"/txt")
     create_dir(path+"/building+shadow_bbox")
+    create_dir(path+"/building_bbox")
     create_dir(path+"/building+shadow_mask")
     create_dir(path+"/building+shadow_bbox_combined")
     create_dir(path+"/building_mask+rgb")
@@ -161,41 +169,46 @@ def main(dest_folder, src_folder, threshold):
         shdw = making_seg_mask(shdw, 255)
         bldg_ftprint = making_seg_mask(bldg_ftprint, 6)
         gt = making_seg_mask(gt, 6)
-
+        cat_two_img(rgb,cv2.cvtColor(gt, IMREAD_COLOR), "RGB + SHDW")
         
-        new_mask = combining_two_masks_or(bldg_ftprint, shdw).astype(np.uint8)
-        new_mask = making_seg_mask(new_mask, 1)
+        # bboxes = mask_to_bbox(shdw)
+        # (string, new_img) = looping_over_bb_boxes(bboxes, rgb, threshold)
+        # cat_two_img(cv2.cvtColor(shdw, IMREAD_COLOR), new_img, "RGB + SHDW")
+        # new_mask = combining_two_masks_or(bldg_ftprint, shdw).astype(np.uint8)
+        # new_mask = making_seg_mask(new_mask, 1)
         
         str1 = ""
         """ Detecting bounding boxes for footprint """
-        bboxes = mask_to_bbox(bldg_ftprint)
+        bboxes = mask_to_bbox(gt)
         (string, new_img) = looping_over_bb_boxes(bboxes, rgb, threshold)
         str1 += string
+        cat_two_img(rgb_copy, new_img, "RGB + GT")
 
-        """ Detecting bounding boxes for shadow """
-        bboxes = mask_to_bbox(shdw)
-        (string, building_shdw_sep_bbox_img) = looping_over_bb_boxes(bboxes, new_img, threshold, color_bbox=(0,0,255))
-        str1 += string
+        # """ Detecting bounding boxes for shadow """
+        # bboxes = mask_to_bbox(shdw)
+        # (string, building_shdw_sep_bbox_img) = looping_over_bb_boxes(bboxes, new_img, threshold, color_bbox=(0,0,255))
+        # str1 += string
 
-        bboxes = mask_to_bbox(new_mask)
-        (string, building_shdw_comb_bbox_img) = looping_over_bb_boxes(bboxes, rgb_copy, threshold, color_bbox=(0,255,0))
+        # bboxes = mask_to_bbox(new_mask)
+        # (string, building_shdw_comb_bbox_img) = looping_over_bb_boxes(bboxes, rgb_copy, threshold, color_bbox=(0,255,0))
         
         # print(len(string.split("\n")))
         # """writing txt file"""
         # file = open("results3/txt/" + name + ".txt", "w")
-        # file.write(string)
+        # file.write(str1)
         # file.close()
         
-        """ Saving the image """
-        cv2.imwrite("results3/building+shadow_mask/" + name + ".png", np.concatenate([cv2.cvtColor(new_mask, IMREAD_COLOR), original], axis=1))
-        cv2.imwrite("results3/building+shadow_bbox/" + name + ".png", building_shdw_sep_bbox_img) 
-        cv2.imwrite("results3/building+shadow_bbox_combined/" + name + ".png", building_shdw_comb_bbox_img)
-        cv2.imwrite("results3/building_mask+rgb/" + name + ".png", np.concatenate([cv2.cvtColor(bldg_ftprint, IMREAD_COLOR), original], axis=1))
+        # """ Saving the image """
+        # cv2.imwrite("results3/building+shadow_mask/" + name + ".png", np.concatenate([cv2.cvtColor(new_mask, IMREAD_COLOR), original], axis=1))
+        # cv2.imwrite("results3/building+shadow_bbox/" + name + ".png", building_shdw_sep_bbox_img) 
+        # cv2.imwrite("results3/building+shadow_bbox_combined/" + name + ".png", building_shdw_comb_bbox_img)
+        # cv2.imwrite("results3/building_mask+rgb/" + name + ".png", np.concatenate([cv2.cvtColor(bldg_ftprint, IMREAD_COLOR), original], axis=1))
+        cv2.imwrite("results3/building_bbox/" + name + ".jpg", new_img)
 
 if __name__ == "__main__":
     """ Constants """
     dest_folder = "results3"
     src_folder = "data3"
-    threshold = 0
+    threshold = 500
     main(dest_folder, src_folder, threshold)
     
