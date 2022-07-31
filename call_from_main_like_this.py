836eaff0d58cdae5d989c88ed28d3ed707eb3277
ShayaanJ <jamilshayaan@gmail.com>
2022-07-31 15:21:40 +0500

    ready to merge with rest of pipeline

--- call_from_main_like_this.py
+++ call_from_main_like_this.py
@@ -1,7 +1,12 @@
 from mask_to_bbox import read_tif, making_seg_mask, its_morphin_time
+import cv2
+import numpy as np
 
-rgb = cv2.imread(rgb, cv2.IMREAD_COLOR)
-gt = read_tif(gt)
+IMAGE_PATH = ""
+ANN_PATH = ""
+
+rgb = cv2.imread(IMAGE_PATH, cv2.IMREAD_COLOR)
+gt = read_tif(ANN_PATH)
 gt = making_seg_mask(gt, 6)
 kernel = np.ones((5, 5), np.uint8)
 (string, eroded_bbox_img) = its_morphin_time(rgb.copy(), gt, kernel,500)
